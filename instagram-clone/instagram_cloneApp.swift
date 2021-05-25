fa1fc24f177842a1653e8346cd16bbe4985b9b47
rodrigo <rodgomesc@gmail.com>
2021-05-25 10:52:46 -0300

    Initial Commit

---
+++ instagram-clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram-clone
+//
+//  Created by Rodrigo Gomes on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
